0c07dd6685f010794abcc2c398592ada7933e9eb
LiamEMM <liam.playradom@gmail.com>
2025-03-25 22:22:22 +0100

    added function for adding 2 numbers

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define PI 3.14159  // Makro za vrednost Ï
+#define PI 3.14159
 
 #define DEBUG 
 
@@ -10,9 +10,16 @@
     #define LOG(x)
 #endif
 
+int sestej(int a, int b) {
+    return a + b;
+}
 
 int main() {
-    LOG("Začenjam program...");
+    int x = 5, y = 10;
+    int vsota = sestej(x, y);
+
+    LOG("ZaÄenjam program...");
+    printf("Vsota %d in %d je %d\n", x, y, vsota);
     printf("Vrednost PI je %.5f\n", PI);
 
     return 0;
